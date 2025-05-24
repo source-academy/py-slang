f26c68c6a467fcdc42638c35d62669256d766add
WangYuyang <127223847+WangYuyang1013@users.noreply.github.com>
2025-05-24 16:55:32 +0800

    Integrate Conductor Interface for py-slang Runner (#56)
    * conductor interface
    
    * acknowledgement
    
    * acknowledgement
    
    ---------
    
    Co-authored-by: Wangdahai <wangdahai@WangdahaideMacBook-Air.local>

---
+++ src/common/Constant.ts
@@ -0,0 +1,8 @@
+// This file is adapted from:
+// https://github.com/source-academy/conductor
+// Original author(s): Source Academy Team
+
+export const enum Constant {
+    PROTOCOL_VERSION = 0,
+    PROTOCOL_MIN_VERSION = 0,
+}
