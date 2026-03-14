5ef88683d75ea2036763d539a1267fd6ee436de1
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2026-03-14 23:04:32 +0800

    Fix mistakes from previous merge resolution

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,7 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import json from '@rollup/plugin-json';
-import commonjs from '@rollup/plugin-commonjs';
-import sourcemaps from 'rollup-plugin-sourcemaps';
 
 /**
  * @type {import('rollup').RollupOptions}
@@ -23,7 +22,6 @@ const config = [
       }),
       json(),
       typescript(),
-      sourcemaps(),
     ],
   },
   {
@@ -41,7 +39,6 @@ const config = [
       }),
       json(),
       typescript(),
-      sourcemaps(),
     ],
   },
 ];
