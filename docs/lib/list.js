0aa56c0b8ad6b085167e6b4873ee13a85c923200
Lucas <nyc@yuchengng.com>
2026-03-16 15:43:42 +0800

    add list.js in docs, equivalent to array.js in js-slang

---
+++ docs/lib/list.js
@@ -0,0 +1,18 @@
+/**
+ * PRIMITIVE
+ * the current length of list x, which is 1 plus the
+ * highest index that has been used so far in a list assignment on
+ * x. Here literal list expressions are counted too: The
+ * list [10, 20, 30] has a length of 3.
+ * @param {list} <CODE>x</CODE> - given value
+ * @returns {int} current length of list
+ */
+function list_length(x) {}
+
+/**
+ * PRIMITIVE
+ * returns True if x is a list and False otherwise.
+ * @param {value} <CODE>x</CODE> - given value
+ * @returns {boolean} whether x is a list
+ */
+function is_list(x) {}
