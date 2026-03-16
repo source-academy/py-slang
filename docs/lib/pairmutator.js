955bdec08e3467e9a8da48eab2added89dc7aa3a
Lucas <nyc@yuchengng.com>
2026-03-16 15:45:39 +0800

    add pairmutator.js in docs/lib

---
+++ docs/lib/pairmutator.js
@@ -0,0 +1,15 @@
+/**
+ * changes the pair p such that its head is x.
+ * @param {pair} <CODE>p</CODE> - given value
+ * @param {value} <CODE>x</CODE> - given value
+ * @returns {undefined} undefined
+ */
+function set_head(p, x) {}
+
+/**
+ * changes the pair p such that its tail is x.
+ * @param {pair} <CODE>p</CODE> - given value
+ * @param {value} <CODE>x</CODE> - given value
+ * @returns {undefined} undefined
+ */
+function set_tail(p, x) {}
