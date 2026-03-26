951079e2a0aa451e2e56b55e056b62766c5a5db1
jonas.ong <jonasong2@gmail.com>
2026-03-26 21:06:24 +0800

    initial conductor support

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import nodeResolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 
 /**
  * @type {import('rollup').RollupOptions}
@@ -40,6 +42,26 @@ const config = [
       json(),
       typescript(),
     ],
+  },
+
+  // wasm
+  {
+    plugins: [
+      nodeResolve({ browser: true }),
+      commonjs({
+        include: "node_modules/**",
+      }),
+      json(),
+      typescript(),
+      nodePolyfills(),
+    ],
+    input: "src/conductor/PyWasmEvaluator.ts",
+    output: {
+      plugins: [terser()],
+      file: "dist/pywasm-evaluator.js",
+      format: "iife",
+      sourcemap: true,
+    },
   },
 ];
 
