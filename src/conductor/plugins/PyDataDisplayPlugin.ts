098a4ab225b4f428bfc8245971c6ef83e89f5e63
Aarav Malani <aarav.malani@gmail.com>
2026-07-28 01:12:59 +0530

    feat: add opaque type to serialiseData

--- src/conductor/plugins/PyDataDisplayPlugin.ts
+++ src/conductor/plugins/PyDataDisplayPlugin.ts
@@ -24,6 +24,7 @@ export default class PyDataDisplayPlugin extends BaseDataDisplayRunnerPlugin<Val
       case "string":
       case "none":
       case "error":
+      case "opaque":
         return { type: "string", value: toPythonString(data, true) };
       case "builtin":
       case "function":
